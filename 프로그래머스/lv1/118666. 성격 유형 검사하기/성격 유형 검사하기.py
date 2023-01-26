5ae0c73cbfbd361a7636280322ef15a63eeace43
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2023-01-26 21:38:27 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -3,56 +3,30 @@ def solution(survey, choices):
     choice = {'R':0,'T':0,'C':0,'F':0,'J':0,'M':0,'A':0,'N':0}
     for i in range(len(survey)):
         if choices[i] > 4:
-            choice[survey[i][1]] += choices[i]%4
+            choice[survey[i][1]] += choices[i] - 4
         elif choices[i] < 4:
-            if choices[i] == 1:
-                choice[survey[i][0]] += 3
-            elif choices[i] == 2:
-                choice[survey[i][0]] += 2
-            else:
-                choice[survey[i][0]] += 1
-                
-        elif choices[i] == 4:
+            choice[survey[i][0]] += 4 - choices[i]
+        else:
             continue
     
-    if choice['R'] > choice['T']:
+    if choice['R'] >= choice['T']:
         answer += 'R'
-    elif choice['R'] < choice['T']:
+    else:
         answer += 'T'
-    else:
-        if ord('T') > ord('R'):
-            answer += 'R'
-        else:
-            answer += 'T'
     
-    if choice['C'] > choice['F']:
+    if choice['C'] >= choice['F']:      #c가 f보다 우선인 것은 알고 있음
         answer += 'C'
-    elif choice['C'] < choice['F']:
+    else:
         answer += 'F'
-    else:
-        if ord('C') > ord('F'):
-            answer += 'F'
-        else:
-            answer += 'C'
 
-    if choice['J'] > choice['M']:
+    if choice['J'] >= choice['M']:
         answer += 'J'
-    elif choice['J'] < choice['M']:
+    else:
         answer += 'M'
-    else:
-        if ord('J') > ord('M'):
-            answer += 'M'
-        else:
-            answer += 'J'
 
-    if choice['A'] > choice['N']:
+    if choice['A'] >= choice['N']:
         answer += 'A'
-    elif choice['A'] < choice['N']:
-        answer += 'N'
     else:
-        if ord('A') > ord('N'):
-            answer += 'N'
-        else:
-            answer += 'A'
+        answer += 'N'
 
     return answer
