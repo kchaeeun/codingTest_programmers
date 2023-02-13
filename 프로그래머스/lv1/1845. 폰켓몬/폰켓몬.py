da9144c867eb565de3f6c9a9ecba523f83c0b90e
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2023-02-14 00:34:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -2,13 +2,10 @@ def solution(nums):
     answer = 0
     get_po = len(nums) // 2
     
-    
     nums=set(nums)
     if get_po < len(nums):
         answer = get_po
-        #answer = (len(nums) * (len(nums) -1)) / get_po
     else:    
         answer = len(nums)
-    
-    
+
     return answer
