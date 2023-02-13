7a5f9265f85b91ad7b1e0f3b404d49483201f89e
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2023-02-13 16:56:36 +0900

    Update 둘만의 암호.py

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,3 +1,15 @@
+#1
+def solution(s,skip,index):
+    res = ''
+    #'a'~'z'까지 중 skip에 없는 리스트 arr --> skip에 있는 문자들을 건너뛸 수 있음
+    arr = [chr(i) for i in range(97, 123) if chr(i) not in skip]
+    # '% len(arr)'를 한 이유 --> index를 더해주고 나서 'z', 122를 넘게되면 다시 'a'로 돌아가 순환하기 위함 
+    for i in s:
+        res += arr[(arr.index(i) + index) % len(arr)]
+
+    return res
+
+#2
 #ascii코드 사용을 위한 라이브러리 import
 from string import ascii_lowercase
 
@@ -15,5 +27,5 @@ def solution(s, skip, index):
 
     for i in s:
         result += a_to_z[(dic_alpha[i] + index) % len(a_to_z)]
-
+    
     return result
