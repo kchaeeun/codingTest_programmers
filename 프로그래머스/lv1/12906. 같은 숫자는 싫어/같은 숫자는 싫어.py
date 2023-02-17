3118c7cb08dd6172eadc2332c5421459f245a018
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2023-02-17 22:23:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 108.28 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,3 +1,4 @@
+#1. my_sol
 def solution(arr):
     hate_same = []
     for i in range(0,len(arr)-1):
@@ -7,3 +8,13 @@ def solution(arr):
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
     return hate_same
+
+#2. other_sol(새로운 list에서 조건 확인)
+def no_continuous(s):
+    # 함수를 완성하세요
+    result = []
+    for c in s:
+        if len(result) == 0 or result[-1] != c:
+            result.append(c)
+
+    return result
