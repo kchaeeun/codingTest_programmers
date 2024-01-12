e587032daf746d4e4b8b7cf9e66855aa7a0c1e5d
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2024-01-12 21:16:40 +0900

    [level Lv.2] Title: 전화번호 목록, Time: 96.33 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,10 @@
 def solution(phone_book):
     answer = True
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                answer = False
+    phone_book.sort()       # 정렬해서 for문을 적게 사용하고, 최소로 돌게 한다
+    for i in range(len(phone_book)-1):
+        idx = len(phone_book[i])
+        # for j in range(len(phone_book)):
+        # if phone_book[i] != phone_book[i+1]:
+        if phone_book[i+1][:idx] == phone_book[i]:
+            return False
     return answer
