ce39765e26dfbe9811c464342865c99720bb4202
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2024-01-12 21:13:59 +0900

    [level Lv.2] Title: 전화번호 목록, Time: 464.98 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,12 @@
 def solution(phone_book):
     answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        idx = len(phone_book[i])
-        # for j in range(len(phone_book)):
-        # if phone_book[i] != phone_book[i+1]:
-        if phone_book[i+1][:idx] == phone_book[i]:
-            return False
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
     return answer
