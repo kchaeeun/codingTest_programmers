6637faff924f83ca77aa22de3fecdc276e04db76
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2024-01-17 00:12:55 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,5 +1,4 @@
 def solution(clothes):
-    # answer = len(clothes)
     cnt = 1
     dic = {}
     for i in range(len(clothes)):
@@ -8,6 +7,18 @@ def solution(clothes):
                    dic[clothes[i][1]].append(clothes[i][0])
 
     for i in dic:
-        cnt *= len(dic[i]) + 1
+        cnt *= len(dic[i]) + 1 # (a 종류의 개수 + 1) * (b 종류의 개수 + 1)
     cnt -= 1
     return cnt
+
+# hash 사용하기
+def solution(clothes):
+    hash_map = {}
+    for cloth, type in clothes:
+        hash_map[type] = hash_map.get(type,0) + 1 # type에 해당하는 count를 새어준다. (기본값이 0)
+    answer = 1
+    print(hash_map)
+    for type in hash_map:
+        answer *= (hash_map[type] + 1)
+        
+    return answer - 1
