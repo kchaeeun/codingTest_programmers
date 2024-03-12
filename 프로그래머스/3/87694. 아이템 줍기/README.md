58ffed9ea0a38f5ff28991bbd4a91bccafd9e0c7
kchaeeun <102590823+kchaeeun@users.noreply.github.com>
2024-03-13 00:55:08 +0900

    [level 3] Title: 아이템 줍기, Time: 0.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.32 ms
+메모리: 10.3 MB, 시간: 0.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 12일 02:53:56
+2024년 03월 13일 00:55:07
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -14,7 +14,7 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
                     graph[i][j] = 0
                 elif graph[i][j] != 0 :
                     graph[i][j] = 1
-                    
+                      
     dxs = [0,0,1,-1]        #?
     dys = [1,-1,0,0]
     queue = deque()
@@ -26,10 +26,12 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
             answer = visited[x][y]//2       # character와 item이 같은 좌표에 있을 수는 없다
             break
         for dx, dy in zip(dxs, dys):
-            nx, ny = dx + x, dy + y
+            print(dx, dy)
+            nx, ny = dx + x, dy + y     # 상하좌우로 이동
+
             if 0 < nx < 102 and 0 < ny < 102 and not visited[nx][ny]:       # visited가 1이 아니라면
-                if graph[nx][ny] == 1:
-                    visited[nx][ny] = visited[x][y] + 1
+                if graph[nx][ny] == 1:              # 선에 위치한 좌표라면
+                    visited[nx][ny] = visited[x][y] + 1     # 경로 추가, x와 y는 character의 위치
                     queue.append((nx, ny))
     return answer
                     
